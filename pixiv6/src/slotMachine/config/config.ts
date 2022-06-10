b8851265e4e18925b232dd918a4e9d3ad44d28bc
Yurii Shchepilov <yuraschepilov@gmail.com>
2022-06-10 18:55:21 +0300

    changed color format from decimal to hex

--- pixiv6/src/slotMachine/config/config.ts
+++ pixiv6/src/slotMachine/config/config.ts
@@ -5,7 +5,7 @@ export const config: ConfigInterface = {
   gameHeight: 800,
   reelWidth: 160,
   symbolSize: 150,
-  betPanelColor: 14919382,
+  betPanelColor: 0xE3A6D6,
   reelsCount: 5,
   symbolsCount: 3,
 };
